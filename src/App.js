160069d672308a36cef4ff17bf85aecac72fbccd
MajemiteJames <majemiteokoro@gmail.com>
2020-09-05 10:00:20 +0100

    fix design and some errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import NewsCards from "./components/NewsCards/NewsCards";
-import { wordsToNumbers } from "words-to-numbers";
 
+import logo from "./images/logo.png";
+import { NewsCards, Modal } from "./components";
 import useStyles from "./styles";
 
-const alanKey =
-  "9b483d8aca488f46385a9608d1538ff32e956eca572e1d8b807a3e2338fdd0dc/stage";
-
-function App() {
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
-  const [activeArticle, setActiveArticle] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
+
   const classes = useStyles();
+  // const alanKey =
+  // "9b483d8aca488f46385a9608d1538ff32e956eca572e1d8b807a3e2338fdd0dc/stage";
+
   useEffect(() => {
     alanBtn({
-      key: alanKey,
+      key:
+        "9b483d8aca488f46385a9608d1538ff32e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
         } else if (command === "highlight") {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
         } else if (command === "open") {
@@ -29,27 +36,63 @@ function App() {
           const article = articles[parsedNumber - 1];
 
           if (parsedNumber > 20) {
-            alanBtn().playText("Please try that again.");
+            alanBtn().playText("Please try that again...");
           } else if (article) {
             window.open(article.url, "_blank");
-            alanBtn().playText("Opening ....");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
           }
         }
       },
     });
   }, []);
+
   return (
     <div>
       <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
         <img
           src="https://alan.app/voice/images/previews/preview.jpg"
           className={classes.alanLogo}
-          alt="alan logo"
+          alt="logo"
         />
       </div>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="https://www.linkedin.com/in/adrian-hajdin/"
+            >
+              {" "}
+              Majemite Okoro
+            </a>{" "}
+          </Typography>
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
